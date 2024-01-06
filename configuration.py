03b471d187e2429efaefa08f5c106597e453cc28
David <martinez.aguirre101126@gmail.com>
2024-01-06 18:11:43 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://d4f05b0a-9728-4b39-9982-5a785eefb8be.serverhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+#Solicitudes POST
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
